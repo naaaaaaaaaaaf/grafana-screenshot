f40fe928ed44987e7def43c224c5091eeb5da3f9
Takato Ueno <naf@auriga.dev>
2023-02-12 22:52:05 +0900

    設定ファイルをiniからjsonに変更

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import time
-import configparser
+import json
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -13,7 +13,7 @@ def is_visible(driver,locator, timeout = 20):
     except TimeoutException:
         return False
 
-def getPanelImage(url):
+def getPanelImage(url,filename):
     driver.get(url)
     driver.set_window_size(900,500)
     driver.implicitly_wait(0.5)
@@ -24,8 +24,7 @@ def getPanelImage(url):
     except:
         driver.quit()
 
-    driver.get_screenshot_as_file(f'aaa.png')
-    driver.quit()
+    driver.get_screenshot_as_file(filename+".png")
     
 def dologin(baseurl,user,password):
     driver.get(f'{baseurl}/login')
@@ -37,15 +36,19 @@ def dologin(baseurl,user,password):
 
 
 if __name__ == "__main__":
-    config = configparser.ConfigParser(interpolation=None)
-    config.read('config/config.ini')
+    f_in = open("config/config.json", "r")
+    config = json.load(f_in)
 
     baseurl = config['grafana']['base_url']
     user = config['grafana']['user']
     password = config['grafana']['password']
 
-    chrome_service = fs.Service(executable_path=config['driver']['driver_path']) 
+    chrome_service = fs.Service(executable_path=config['driver_path']) 
     driver = webdriver.Chrome(service=chrome_service)
     dologin(config['grafana']['base_url'],user,password)
     time.sleep(1)
-    getPanelImage(config['grafana']['panel_url'])
+
+    for i in config['grafana']['panel']:
+        getPanelImage(i['url'],i['name'])
+    
+    driver.quit()
